cdcc19d1577ef0134598d5b939399581ec3d0af9
Gaëtan H <me@gaetanhus.fr>
2026-06-05 02:21:42 +0200

    fix(companion-window.tsx): update image source and dimensions for the companion window to improve visual quality and layout

--- apps/web/components/home/companion-window.tsx
+++ apps/web/components/home/companion-window.tsx
@@ -8,9 +8,9 @@ export const CompanionWindow: FC = (): ReactElement => {
     <div className="perspective-[1000px] relative">
       <div className="overflow-hidden transform rotate-y-[-5deg] rotate-x-2 animate-float min-h-50 flex items-center justify-center">
         <Image
-          src="/assets/extension.png"
+          src="/assets/extension_1.png"
           alt="Companion Window"
-          width={200}
+          width={500}
           height={100}
           className="object-cover w-82"
           loading="eager"
