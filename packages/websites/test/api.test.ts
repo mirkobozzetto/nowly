c58678fb34068de116d3dfdae213b16243d44f9a
Gaëtan H <me@gaetanhus.fr>
2026-06-09 01:46:15 +0200

    chore: update version numbers to 1.0.0 for api, extension, and web packages to reflect the release of a stable version style(api.test.ts): reorder import statements for consistency and clarity

--- packages/websites/test/api.test.ts
+++ packages/websites/test/api.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect } from "vitest"
+import { describe, expect, it } from "vitest"
 import { bumpVersion, getLocalBundle } from "../cli/api"
 
 describe("bumpVersion", () => {
