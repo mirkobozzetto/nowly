449d952b0de4fabd50130ecc108bff62e8e71054
Gaëtan H <me@gaetanhus.fr>
2026-06-20 20:49:30 +0200

    refactor(universe.ts): remove unused image processing logic to simplify code and improve readability

--- packages/websites/src/G/GitHub/utils/universe.ts
+++ packages/websites/src/G/GitHub/utils/universe.ts
@@ -1,18 +1,15 @@
 import { PresenceType, type PresenceInstance } from "@nowly/presence"
 import { createButton, getTitle } from "./dom"
-import { getMetaImage, toDiscordImage } from "./images"
 import { ProductAssets } from "./products"
 
 export const handleGitHubUniverse = async (
   presence: PresenceInstance,
   href: string,
 ): Promise<void> => {
-  const image = await toDiscordImage(getMetaImage())
-
   await presence.setActivity({
     details: "Browsing GitHub Universe",
     state: getTitle("GitHub Universe"),
-    largeImageKey: image || ProductAssets.UniverseLogo,
+    largeImageKey: ProductAssets.UniverseLogo,
     largeImageText: "GitHub Universe",
     type: PresenceType.Watching,
     buttons: [createButton("View GitHub Universe", href)],
